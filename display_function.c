bcf0ee258dc637ca1d88ac9b127b456bf1ef551d
Ziad Mohamed <132141501+SolSteel@users.noreply.github.com>
2023-09-14 10:48:49 +0300

    Update display_function.c

--- display_function.c
+++ display_function.c
@@ -6,16 +6,6 @@ extern unsigned char mins;
 extern unsigned char hours;
 void display()
 {
-	if(secs==60)
-	{
-		mins++;
-		secs=0;
-	}
-	if(mins==60)
-	{
-		hours++;
-		mins=0;
-	}
 	PORTA=(PORTA&0xC0)|(1<<PA0);//ONLY FIRST SEVEN SEGMENT ON;
 	PORTC=(PORTC&0xF0)|(secs%10); //LET THE VALUE ON DISPLAY = SECS%10
 	_delay_ms(2);
@@ -34,5 +24,4 @@ void display()
 	PORTA=(PORTA&0xC0)|(1<<PA5);//ONLY SECOND SEVEN SEGMENT ON;
 	PORTC=(PORTC&0xF0)|(hours/10); //LET THE VALUE ON DISPLAY = HOURS/10
 	_delay_ms(2);
-
 }
